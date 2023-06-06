57a2a548d2d1ae9a598724c1d8eddf0d5a9626cf
Devynne Marshall <devynnem@gmail.com>
2023-06-06 15:57:54 -0600

    feat:add in the best api

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,5 @@
 const acquireInfo = () => {
-return fetch(`https://api.disneyapi.dev/character?page=3&pageSize=50`)
+return fetch(`https://api.disneyapi.dev/character?page=6&pageSize=50`)
   .then(response => {
     if (!response.ok) {
       throw new Error(`${response.status}`)
@@ -10,3 +10,8 @@ return fetch(`https://api.disneyapi.dev/character?page=3&pageSize=50`)
 }
 
 export default acquireInfo;
+
+
+// https://api.disneyapi.dev/character?page=3&pageSize=50`
+// https://api.disneyapi.dev/character?page=6&pageSize=50
+// https://api.disneyapi.dev/character?page=60&pageSize=50`
