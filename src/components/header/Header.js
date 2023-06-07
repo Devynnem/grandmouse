ce563bb270475a62046b27fe748fbc56be8e5315
Devynne Marshall <devynnem@gmail.com>
2023-06-07 17:04:48 -0600

    feat:Add conditional rendering if there is no data available and also more styling everywhere

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,14 +1,15 @@
 import "./Header.css";
+import { Link } from 'react-router-dom'
 import logo from '../../assets/Mickey-Mouse-Ears-Clip-Art-N34.png'
 
 const Header = () => {
   return (
     <header className="header-container">
-      {/* <Link to="/"> */}
+      <Link to="/">
         <img src={logo} id="header-logo" alt="GrandMouse logo" />  
+      </Link>
         <h1>GrandMouse</h1>
-      {/* </Link> */}
-      {/* <h1 className="slogan">Elevate your beauty game with Glam Squad!</h1> */}
+      <p className="slogan">Welcome new and existing grandparents to a place to keep up with the grandkids favorite character!</p>
     </header>
   );
 };
--- src/components/singleCharacterCard/SingleCharacterCard.js
+++ src/components/singleCharacterCard/SingleCharacterCard.js
@@ -1,25 +1,17 @@
 import React from 'react';
 import './SingleCharacterCard.css';
-import { Link } from 'react-router-dom';
 
 
 
 const SingleCharacterCard = ({ character }) => {
-  // console.log("single character", character.imageURL)
+  // console.log(character.films)
   return (
     <div className="single-character-card">
       <img className="single-img"src={character.imageUrl} />
       <div className="character-info">
         <h2 className="character-name">{character.name}</h2>
-        <p>Films:   {character.films}</p>
-        <p>TV Shows:   {character.tvShows}</p>
-      {/* <p>Tagline: {character.tagline}</p>
-      <p>Plot: {character.overview}</p>
-      <p>Runtime: {character.runtime} min</p>
-      <p>Average Rating: {characterRating}</p>
-      <p>Budget: ${character.budget}</p>
-      <p>Revenue: ${character.revenue}</p> */}
-    {/* <Link to={'/'} key={`${character.id}`}><button className="back-to-main" onClick= {() => displayMainPage()}>Back to Movies</button></Link> */}
+        {character.films.length === 0 ? (<p>No films yet!</p>) : (<p>Films:   {character.films}</p>)}
+        {character.tvShows.length === 0 ? (<p>No TV Shows yet!</p>) : (<p>TV Shows:   {character.tvShows}</p>)}
       </div>
   </div>
   )
