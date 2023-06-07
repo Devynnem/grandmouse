5a13fb01f6839093fee4094ce6f8fe06870fe34c
Devynne Marshall <devynnem@gmail.com>
2023-06-06 23:15:44 -0600

    feat:Add more styling to the main page

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,5 @@
 const acquireInfo = () => {
-return fetch(`https://api.disneyapi.dev/character?page=6&pageSize=50`)
+return fetch(`https://api.disneyapi.dev/character?page=60&pageSize=50`)
   .then(response => {
     if (!response.ok) {
       throw new Error(`${response.status}`)
